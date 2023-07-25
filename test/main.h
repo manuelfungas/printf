98b1f1021ebb93a425abebf71ecbaa0570981e4f
Manuel Rombe <manuelfungatejr@gmail.com>
2023-07-25 19:45:40 +0200

    gain exp by living

--- test/main.h
+++ test/main.h
@@ -3,6 +3,8 @@
 
 int _printf(const char *format, ...);
 
+ int new_printf(const char *format, ...);
+
 int _putchar(char c);
 
 #endif /* MAIN_H */
---
+++ test/new_print.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdarg.h>
+
+int new_printf(const char *format, ...)
+{ int new_printf(const char *format, ...)
+
+	int len = 0;
+	char *buffer;
+	va_list args;
+
+	va_start(args, format);
+
+	while (*format)
+	{
+		if (*format != '%')
+		{
+			_putchar(*format);
+			len = len + 1;
+		}
+		else
+		{
+			format++;
+
+			switch (*format)
+			{
+				case 'c':
+					char c = (char)va_args(buffer, int);
+					_putchar(c);
+					len = len + 1;
+					break;
+				case 's':
+					buffer = va_arg(args, char *);
+					while (*buffer)
+					{
+						_putchar(*buffer);
+						buffer;
+						count++;
+					}
+					break;
+				case '%':
+					putchar('%');
+					len = len + 1;
+					break;
+				default:
+					_putchar('%');
+					_putchar(ch);
+					len = len + 2;
+			}
+		}
+		format++
+	}
+	va_end(args);
+
+	return (len);
+}
