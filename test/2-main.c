54da86522aa8ef7e4f947d38d85fa8d0b9ed80bf
tjlouw <thornelouw@gmail.com>
2023-07-25 23:00:07 +0200

    more good test files

---
+++ test/2-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point, testing for percent value
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char prc = '%';
+	printf("%%\n", prc);
+	new_printf("%%\n", prc);
+	return (0);
+}
