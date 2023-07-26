71279d7e4e6670e77564d500d93ab8b7b3939cff
Manuel Rombe <manuelfungatejr@gmail.com>
2023-07-26 19:45:17 +0200

    Small fix

--- test/main.h
+++ test/main.h
@@ -2,9 +2,9 @@
 #define MAIN_H
 #include <stdarg.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 int _printf(const char *format, ...);
-int new_printf(const char *format, ...);
-int _putchar(char c);
 
 #endif /* MAIN_H */
