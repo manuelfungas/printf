000c163c8bc923190d170bf635883545fdd00bd2
Manuel Rombe <manuelfungatejr@gmail.com>
2023-07-26 21:35:23 +0200

    handling edge cases

--- _printf.c
+++ _printf.c
@@ -51,6 +51,11 @@ int print_str(char *s)
 	int count;
 
 	count = 0;
+	if (s == NULL)
+	{
+		count += write(STDOUT_FILENO, "(null)", 6);
+        return (count);
+	}
 	while (*s)
 	{
 		count += write(1, s++, 1);
