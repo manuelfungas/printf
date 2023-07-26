dec12eb6e370bf3fea7397a0918726cf707357a9
tjlouw <thornelouw@gmail.com>
2023-07-26 02:33:25 +0200

    final test

---
+++ test/4-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main -Entry point, final test
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	float num = 19.99;
+
+	printf("%f\n", num);
+	new_printf("%f\n", num);
+	return (0);
+}
+
