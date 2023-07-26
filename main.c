5596041a4fa79f2477f77bc05fff8e13e0d821ec
tjlouw <thornelouw@gmail.com>
2023-07-26 13:23:43 +0200

    main.c code from test

--- main.c
+++ main.c
@@ -1,4 +1,3 @@
-#include <limits.h>
 #include <stdio.h>
 #include "main.h"
 
@@ -9,36 +8,25 @@
  */
 int main(void)
 {
-    int len;
-    int len2;
-    unsigned int ui;
-    void *addr;
+	char str[] = "if this works, all is well";
+	char first_ch;
+	char prc;
+	int found = 2019, curr = 2023;
+	float num = 19.99;
 
-    len = _printf("Let's try to printf a simple sentence.\n");
-    len2 = printf("Let's try to printf a simple sentence.\n");
-    ui = (unsigned int)INT_MAX + 1024;
-    addr = (void *)0x7ffe637541f0;
-    _printf("Length:[%d, %i]\n", len, len);
-    printf("Length:[%d, %i]\n", len2, len2);
-    _printf("Negative:[%d]\n", -762534);
-    printf("Negative:[%d]\n", -762534);
-    _printf("Unsigned:[%u]\n", ui);
-    printf("Unsigned:[%u]\n", ui);
-    _printf("Unsigned octal:[%o]\n", ui);
-    printf("Unsigned octal:[%o]\n", ui);
-    _printf("Unsigned hexadecimal:[%x, %X]\n", ui, ui);
-    printf("Unsigned hexadecimal:[%x, %X]\n", ui, ui);
-    _printf("Character:[%c]\n", 'H');
-    printf("Character:[%c]\n", 'H');
-    _printf("String:[%s]\n", "I am a string !");
-    printf("String:[%s]\n", "I am a string !");
-    _printf("Address:[%p]\n", addr);
-    printf("Address:[%p]\n", addr);
-    len = _printf("Percent:[%%]\n");
-    len2 = printf("Percent:[%%]\n");
-    _printf("Len:[%d]\n", len);
-    printf("Len:[%d]\n", len2);
-    _printf("Unknown:[%r]\n");
-    printf("Unknown:[%r]\n");
-    return (0);
+	first_ch = 'f';
+	prc = '%';
+	printf("%s\n", str);
+	_printf("%s\n", str);
+	printf("%%\n", prc);
+	_printf("%%\n", prc);
+	printf("%c\n", first_ch);
+	_printf("%c\n", first_ch);
+	printf("%d\n", found);
+	_printf("%d\n", found);
+	printf("%i\n", curr);
+	_printf("%i\n", curr);
+	printf("%f\n", num);
+	_printf("%f\n", num);
+	return (0);
 }
